69c9ca7cc5c41c904309e452fc146d88740abf7d
Pippin Williamson <pippin@pippinsplugins.com>
2017-12-21 12:13:11 -0600

    Remove slim body class on pages.

--- sugar-calendar-functionality.php
+++ sugar-calendar-functionality.php
@@ -8,3 +8,15 @@
  * Version: 1.0
  */
 
+function sc_remove_slim_body_class( $classes ) {
+
+	$found = array_search( 'slim', $classes );
+
+	if( is_page() && false !== $found ) {
+		unset( $classes[ $found ] );
+	}
+
+	return $classes;
+
+}
+add_filter( 'body_class', 'sc_remove_slim_body_class' );
