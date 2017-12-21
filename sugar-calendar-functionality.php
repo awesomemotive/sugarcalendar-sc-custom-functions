d6a944979d787786b7eec8156a55f69c78fbc1c4
Pippin Williamson <pippin@pippinsplugins.com>
2017-12-21 12:01:30 -0600

    Add initial plugin file

---
+++ sugar-calendar-functionality.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Sugar Calendar - Site Functionality
+ * Plugin URI: https://sugarcalendar.com
+ * Description: Custom functionality for Sugar Calendar website.
+ * Author: Easy Digital Downloads
+ * Author URI: https://sugarcalendar.com
+ * Version: 1.0
+ */
+
