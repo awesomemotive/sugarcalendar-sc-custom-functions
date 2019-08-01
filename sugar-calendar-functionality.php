7dfd6f1d841318d0c902c4c49ac8208a167c8a62
Pippin Williamson <pippin@pippinsplugins.com>
2019-08-01 13:36:24 -0500

    Enable all categories in access passes

--- sugar-calendar-functionality.php
+++ sugar-calendar-functionality.php
@@ -5,7 +5,7 @@
  * Description: Custom functionality for Sugar Calendar website.
  * Author:      Sandhills Development, LLC
  * Author URI:  https://sandhillsdev.com
- * Version:     1.0.0
+ * Version:     1.0.1
  */
 
 /**
@@ -28,3 +28,25 @@ function sc_remove_slim_body_class( $classes = array() ) {
 	return $classes;
 }
 add_filter( 'body_class', 'sc_remove_slim_body_class', 11 );
+
+/**
+ * Enables all add-on categories for All Access Passes
+ *
+ * @since 1.0.1
+ *
+ * @param array $classes
+ *
+ * @return array
+ */
+function sc_custom_aa_categories( $included_categories, $all_access_pass_object ) {
+
+    $download_id = $all_access_pass_object->download_id;
+    $price_id = $all_access_pass_object->price_id;
+
+    if ( 21 === $download_id || 20 === $download_id ) {
+        $included_categories = array( 7, 2, 5 );
+    }
+
+    return $included_categories;
+}
+add_filter( 'edd_all_access_included_categories', 'sc_custom_aa_categories' );
